f348fc175de685cfad2c5c26d46dd05849a1b7be
Ke Liu <ke.frank.liu@gmail.com>
2015-12-15 22:21:57 +1100

    unfinished ProductController changes.

--- MyStore.Server/MyStore.Server.WebApi/Controllers/ProductController.cs
+++ MyStore.Server/MyStore.Server.WebApi/Controllers/ProductController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Windows.Input;
 using Product.ReadModel;
 
 namespace MyStore.Server.WebApi.Controllers
@@ -6,13 +10,42 @@ namespace MyStore.Server.WebApi.Controllers
     public class ProductController : ApiController
     {
         private readonly IProductDao productDao;
+        private readonly ICommand _bus;
 
-        public ProductController(IProductDao productDao)
+        public ProductController(IProductDao productDao, ICommand bus)
         {
+            _bus = bus;
             productDao = productDao;
         }
 
+        [ResponseType(typeof (Product))]
+        public async Task<IHttpActionResult> GetProduct(Guid productId)
+        {
+            var product = productDao.FindProduct(productId);
 
+            if (product == null)
+            {
+                return NotFound();
+            }
 
+            return Ok(product);
+        }
+
+        public async Task<IHttpActionResult> LocateProductId(string name)
+        {
+            var productId = productDao.LocateProduct(name);
+
+            if (!productId.HasValue)
+            {
+                return NotFound();
+            }
+
+            return Ok(productId.Value);
+        }
+
+        public async Task<IHttpActionResult> CreateProduct(string name, Guid brandId, )
+        {
+            var command = new CreateProduct
+        }
     }
 }
