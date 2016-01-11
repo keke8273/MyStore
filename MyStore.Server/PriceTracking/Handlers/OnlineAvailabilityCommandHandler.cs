b362cd48bc3c3a30f9778e5d53ebce79ace26481
Frank Liu <ke.frank.liu@gmail.com>
2016-01-11 23:47:49 +1100

    adding untracked files.

---
+++ MyStore.Server/PriceTracking/Handlers/OnlineAvailabilityCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.Infrastructure.EventSourcing;
+using CQRS.Infrastructure.Messaging.Handling;
+using MyStore.Common;
+using ProductTracking.Commands;
+
+namespace ProductTracking.Handlers
+{
+    public class OnlineAvailabilityCommandHandler:
+        ICommandHandler<UpdateProductOnlineAvailability>
+    {
+        private readonly IEventSourcedRepository<ProductOnlineAvailability> _repository;
+        private readonly IDateTimeService _dateTimeService;
+
+        public OnlineAvailabilityCommandHandler(IEventSourcedRepository<ProductOnlineAvailability> repository, IDateTimeService dateTimeService)
+        {
+            this._repository = repository;
+            _dateTimeService = dateTimeService;
+        }
+
+        public void Handle(UpdateProductOnlineAvailability command)
+        {
+            var availability = _repository.Find(command.ProductId);
+
+            if (availability == null)
+                availability = new ProductOnlineAvailability(command.ProductId);
+
+            availability.UpdateOnlineAvailability(command.ProductSourceId, command.IsAvailable, _dateTimeService);
+
+            _repository.Save(availability, command.Id.ToString());
+        }
+    }
+}
---
+++ MyStore.Server/PriceTracking/Handlers/ProductPriceCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using CQRS.Infrastructure.EventSourcing;
+using CQRS.Infrastructure.Messaging.Handling;
+using MyStore.Common;
+using ProductTracking.Commands;
+
+namespace ProductTracking.Handlers
+{
+    public class ProductPriceCommandHandler :
+        ICommandHandler<UpdateProductPrice>
+    {
+        private readonly IEventSourcedRepository<ProductPrice> _repository;
+        private readonly IDateTimeService _dateTimeService;
+
+        public ProductPriceCommandHandler(IEventSourcedRepository<ProductPrice> repository, IDateTimeService dateTimeService)
+        {
+            this._repository = repository;
+            _dateTimeService = dateTimeService;
+        }
+
+        public void Handle(UpdateProductPrice command)
+        {
+            var productPrice = _repository.Find(command.ProductId);
+
+            if (productPrice == null)
+                productPrice = new ProductPrice(command.ProductId);
+
+            productPrice.UpdatePrice(command.ProductSourceId, command.Price, _dateTimeService);
+
+            _repository.Save(productPrice, command.Id.ToString());
+        }
+    }
+}
