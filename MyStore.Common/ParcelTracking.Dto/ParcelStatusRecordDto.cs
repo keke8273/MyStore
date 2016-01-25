56110ba41711965d0c9dfbebb8f5b2a2e4f0599c
Frank Liu <ke.frank.liu@gmail.com>
2016-01-25 21:47:31 +1100

    renamed the ParcelTracing.Dto project directory

---
+++ MyStore.Common/ParcelTracking.Dto/ParcelStatusRecordDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ParcelTracking.Dto
+{
+    public class ParcelStatusRecordDto
+    {
+        public DateTime TimeStamp { get; set; }
+        public string Location { get; set; }
+        public string Message { get; set; }
+    }
+}
